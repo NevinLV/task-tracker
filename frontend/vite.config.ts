c83a085fddf89eba56ea5a3a966eed60bdc44db4
Lebedeff <32606109+NevinLV@users.noreply.github.com>
2026-04-01 18:41:33 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': { target: 'http://127.0.0.1:8000', changeOrigin: true },
-      '/static': { target: 'http://127.0.0.1:8000', changeOrigin: true },
+      '/api': { target: 'https://task-tracker-5nzw.onrender.com', changeOrigin: true },
+      '/static': { target: 'https://task-tracker-5nzw.onrender.com', changeOrigin: true },
     },
   },
 })
